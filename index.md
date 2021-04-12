6d18a3bd442eefdfbcfa32cdf6fe0b7f4c98c060
dcadata <39072469+dcadata@users.noreply.github.com>
2021-04-11 23:09:35 -0400

    restructure

--- index.md
+++ index.md
@@ -1,66 +1,16 @@
-# Link Archive and Unofficial RSS Feed for *This American Life* Podcast Episodes 1 through Current
+# *This American Life* Podcast - Unofficial RSS Feed - Episodes 1 to Current
 
-## ***ANNOUNCEMENT***
+---
 
-### As of 08 Sep 2020, THIS FEED IS NO LONGER MAINTAINED. PLEASE USE THE FEED AT (https://static.awk.space/tal.xml)[https://static.awk.space/tal.xml] (no affiliation). Thank you!
+- **[Unofficial RSS feed](http://tal.ankar.io)** that you can subscribe to with your RSS reader/podcatcher/podcast app of choice.
 
-***
+- [View on GitHub](https://github.com/dcadata/this-american-life-archive/) - download CSVs, check missing episodes, etc.
+  
+---
 
-* ## [Unofficial RSS Feed](http://dcadata.github.io/this-american-life-archive/TALArchive.xml)
-  * ### This is a valid RSS feed that you can subscribe to with your podcatcher of choice!
-  * Unofficial RSS feed for the "This American Life" podcast, going back to the very first episode! Updated Mondays in MP3 format. Visit [ThisAmericanLife.org](http://www.thisamericanlife.org) for more information about the podcast.
-  * [View XML on GitHub](https://github.com/dcadata/this-american-life-archive/blob/master/TALArchive.xml)
+*[This American Life](https://www.thisamericanlife.org/about)* is a radio show/podcast. The podcast's [official RSS feed](http://feed.thisamericanlife.org/talpodcast) only serves the most recent episodes. However, all podcast episodes are publicly available on the official website, even after they fall off the official RSS feed, which makes this project possible.
 
-* **CSV - [view on GitHub](https://github.com/dcadata/this-american-life-archive/blob/master/TALArchive.csv) or [download](TALArchive.csv)**
+---
 
-* [Index/table of all episodes](https://github.com/dcadata/this-american-life-archive/blob/master/TALArchive.md)
+(This project was discontinued in September 2020 and restarted in April 2021.)
 
-***
-
-## What is this?
-
-*[This American Life](http://www.thisamericanlife.org/)* is a radio show/podcast published by WBEZ Chicago Public Radio. The podcast's [official RSS feed](http://feed.thisamericanlife.org/talpodcast) only serves the most recent 5-10 episodes. However, all podcast episodes are freely and publicly available on the podcast's official website. Older episodes fall off the official RSS feed, but they are still freely available.
-
-This project simply scrapes the URLs for those older podcast episodes that are still available but have fallen off the official RSS feed. (I am not, and will not be, hosting any *This American Life* audio files. This is only an archive of *links*, not *files*!)
-
-***
-
-## Process
-
-This project was completed by automatically scraping up to 14 URLs for each of almost 700 episodes until an audio file was found for each episode in the archive. I essentially automated a process many listeners already do manually when they want to listen to an older episode. A given episode's audio file may sit at one of 14 different URLs, and the URL choice varies from episode to episode.
-
-I also used the official short URL (`tal.fm/{episode number}`) to scrape information about each episode, currently including the episode title, description, and published date/airdate. File URLs and info for new episodes will be collected from the official RSS feed as new episodes are released.
-
-***
-
-## FAQ
-
-### Why are some episodes missing?
-
-[A list of missing episodes is here.](https://github.com/dcadata/this-american-life-archive/blob/master/ref_missing_eps_list.txt)
-
-Each week, the script attempts to automatically clear the missing episodes by finding audio file URLs for them.
-
-11/11/2019- An error was previously made in identifying missing episodes. Missing episodes can now be identified (if they arise) and will attempt to be cleared each week.
-
-### Why are there multiple entries for some episodes?
-
-~~If an episode's audio file is accessible at multiple URLs, each URL is listed as a separate entry for the same episode. That way, you have your choice of URLs if one goes down.~~
-
-As of 7/31/2019, each episode now only has one URL. If that URL goes down, it will automatically find another one on the next run (the following Sunday evening).
-
-### What if an episode becomes inaccessible in the future?
-
-This is a possibility. For now, I have a script that checks back through old file URLs each week to make sure they are still accessible. If not, it will check the other 13 possible URLs until it finds a copy of the file.
-
-***
-
-**Tools Used:** Python - requests, Beautiful Soup, Pandas
-
-***
-
-**Contact:** [devon@ankar.io](mailto:devon@ankar.io)
-
-***
-
-**Last Updated:** Nov. 11, 2019
---
+++ task.py
@@ -0,0 +1,153 @@
+from datetime import datetime
+from time import sleep
+import pandas as pd
+from bs4 import BeautifulSoup as BS
+from requests import get
+
+
+class Reader:
+    _data_dir = 'data/'
+    _raw_fp = _data_dir + 'raw.csv'
+    _transformed_fp = _data_dir + 'transformed.csv'
+    _exceptions_fp = _data_dir + 'missing.csv'
+
+    @property
+    def raw(self):
+        dtypes = self._dtypes.copy()
+        del dtypes['pubdate_timestamp']
+        return pd.read_csv(self._raw_fp, dtype=dtypes)
+
+    @property
+    def _transformed(self):
+        return pd.read_csv(self._transformed_fp, dtype=self._dtypes)
+
+    @property
+    def _exceptions(self):
+        return pd.read_csv(self._exceptions_fp, dtype={'num': int, 'exc': str})
+
+    @property
+    def _dtypes(self):
+        return {
+            'num': int,
+            'url': str,
+            'full_url': str,
+            'title': str,
+            'description': str,
+            'pubdate': str,
+            'pubdate_timestamp': int,
+            'download_url': str,
+        }
+
+    @property
+    def _str_fields(self):
+        return ['url', 'full_url', 'title', 'description', 'pubdate', 'download_url']
+
+
+class Requester(Reader):
+    def __init__(self, **kwargs):
+        self._nums = kwargs.get('nums')
+        self._new = []
+        self._exc = []
+
+    def make_requests(self):
+        for num in self._nums:
+            try:
+                self._new.append(_make_one_request(num))
+            except Exception as exc:
+                self._exc.append({
+                    'num': num,
+                    'exc': str(exc),
+                })
+            sleep(1)
+
+    def save_raw(self, overwrite=None):
+        new = pd.DataFrame(self._new)
+        df = new if overwrite else pd.concat((new, self.raw))
+        df.to_csv(self._raw_fp, index=False)
+        pd.DataFrame(self._exc).to_csv(self._exceptions_fp, index=False)
+
+
+class Episode:
+    def __init__(self, **kwargs):
+        self._text = kwargs.get('text')
+
+    @property
+    def data(self):
+        title_section = self._soup.find('div', class_='episode-title')
+        container = title_section.find_parent('div', class_='container')
+
+        try:
+            description = container.find('div', class_='field-name-body').text
+        except AttributeError:
+            description = ''
+
+        data = {
+            'pubdate': container.find('div', class_='meta').find('div', class_='field-name-field-radio-air-date').find(
+                'span', class_='date-display-single').text,
+            'title': title_section.find('h1').text,
+            'description': description,
+            'download_url': container.find('ul', class_='actions').find('li', class_='download').find('a').get('href'),
+        }
+        return data
+
+    @property
+    def _soup(self):
+        return BS(self._text, 'lxml')
+
+
+class Writer(Requester):
+    def transform_and_write(self):
+        df = self._transform()
+        df.to_csv(self._transformed_fp, index=False)
+        open('TALArchive.xml', 'w').write(self._write_xml(df))
+
+    def _transform(self):
+        df = self.raw.copy()
+        for col in self._str_fields:
+            df[col] = df[col].fillna(' ').apply(lambda x: x.strip()).apply(lambda x: x.replace('\u02bc', ''))
+        df.download_url = df.download_url.apply(lambda x: x.split('?', 1)[0])
+        df['pubdate_timestamp'] = df.pubdate.apply(pd.to_datetime).apply(lambda x: x.timestamp()).apply(int)
+        df = df.sort_values('pubdate_timestamp', ascending=False).drop_duplicates(subset=['num'])
+        return df[list(self._dtypes)]
+
+    @staticmethod
+    def _write_xml(df):
+        _read = lambda x: open(f'templates/{x}.xml').read()
+        item_xml = _read('item')
+        items_xml = '\n'.join((item_xml.format(**record) for record in df.to_dict('records')))
+        xml_output = _read('feed').format(
+            items=items_xml, feed_last_updated=datetime.today().strftime('%Y-%m-%d %H:%M:%S'))
+        return xml_output
+
+
+def _make_one_request(num):
+    url = f'https://www.thisamericanlife.org/episode/{num}'
+    r = get(url)
+    assert r.ok
+    data = {
+        'num': num,
+        'url': url,
+        'full_url': r.url,
+    }
+    data.update(Episode(text=r.text).data)
+    return data
+
+
+def _get_latest_episode_number():
+    r = get('http://feed.thisamericanlife.org/talpodcast')
+    sleep(1)
+    soup = BS(r.text, 'lxml')
+    return int(soup.find('item').find('title').text.split(':', 1)[0])
+
+
+def main():
+    latest_num = _get_latest_episode_number()
+    writer = Writer(nums=(latest_num,))
+    if latest_num not in set(writer.raw.num):
+        writer.make_requests()
+        writer.save_raw()
+    writer.transform_and_write()
+
+
+if __name__ == '__main__':
+    main()
