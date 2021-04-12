4ab66a6b133863c4a9ad7f04cc223adcfd9dd8aa
dcadata <39072469+dcadata@users.noreply.github.com>
2021-04-12 00:47:59 -0400

    dt

--- task.py
+++ task.py
@@ -109,8 +109,9 @@ class Writer(Requester):
             df[col] = df[col].fillna(' ').apply(lambda x: x.strip()).apply(escape).apply(
                 lambda x: x.replace('\u02bc', ''))
         df.download_url = df.download_url.apply(lambda x: x.split('?', 1)[0])
-        df.pubdate = df.pubdate.apply(pd.to_datetime).apply(lambda x: x.strptime('%a, %d %b %Y %H:%M:%S %z'))
+        df.pubdate = df.pubdate.apply(pd.to_datetime)
         df['pubdate_timestamp'] = df.pubdate.apply(lambda x: x.timestamp()).apply(int)
+        df.pubdate = df.pubdate.apply(lambda x: x.strftime('%a, %d %b %Y %H:%M:%S %z'))
         df = df.sort_values('pubdate_timestamp', ascending=False).drop_duplicates(subset=['num'])
         return df[list(self._dtypes)]
 
