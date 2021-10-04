54b1608cd3a284e7e89ffb4d9029690ab6e0df39
Devon Ankar <devon.ankar@gmail.com>
2021-10-04 10:40:12 -0400

    create method get_nums_to_request

--- task.py
+++ task.py
@@ -44,15 +44,18 @@ class TALScraper:
 
     def __init__(self, **kwargs):
         self.session = kwargs.get('session')
-        self.nums = kwargs.get('nums')
+        self.nums = None
         self._new = []
         self._exc = []
 
-    def get_feed_episode_nums(self):
-        r = self.session.get('http://feed.thisamericanlife.org/talpodcast')
-        sleep(1)
-        soup = BeautifulSoup(r.text, 'lxml')
-        return {int(elem.find('title').text.split(':', 1)[0]) for elem in soup.find_all('item')}
+    def get_nums_to_request(self):
+        completed = self.transformed.copy()
+        completed_nums = set(completed.num)
+        temp_url_nums = set(completed[~completed.download_url.str.contains('thisamericanlife.org')].num)
+        feed_nums = self._get_feed_episode_nums()
+
+        self.nums = set(range(1, max(completed_nums.union(feed_nums)) + 1)) - completed_nums
+        self.nums.update(temp_url_nums - feed_nums)
 
     def make_requests(self):
         for num in self.nums:
@@ -71,6 +74,12 @@ class TALScraper:
         df.to_csv(self._transformed_fp, index=False)
         xml_output = self._write_xml(df)
         open('TALArchive.xml', 'w').write(xml_output)
+
+    def _get_feed_episode_nums(self):
+        r = self.session.get('http://feed.thisamericanlife.org/talpodcast')
+        sleep(1)
+        soup = BeautifulSoup(r.text, 'lxml')
+        return {int(elem.find('title').text.split(':', 1)[0]) for elem in soup.find_all('item')}
 
     def _make_one_request(self, num):
         url = f'https://www.thisamericanlife.org/episode/{num}'
@@ -136,23 +145,15 @@ class TALScraper:
 
 
 def main():
-    scraper = TALScraper()
-    scraper.session = Session()
+    self = TALScraper()
+    self.session = Session()
+    self.get_nums_to_rsequest()
+    if self.nums:
+        self.make_requests()
+        self.save_raw_and_exceptions()
+    self.transform_and_write()
 
-    completed = scraper.transformed.copy()
-    completed_nums = set(completed.num)
-    temp_url_nums = set(completed[~completed.download_url.str.contains('thisamericanlife.org')].num)
-    feed_nums = scraper.get_feed_episode_nums()
-
-    scraper.nums = set(range(1, max(completed_nums.union(feed_nums)) + 1)) - completed_nums
-    scraper.nums.update(temp_url_nums - feed_nums)
-
-    if scraper.nums:
-        scraper.make_requests()
-        scraper.save_raw_and_exceptions()
-    scraper.transform_and_write()
-
-    scraper.session.close()
+    self.session.close()
 
 
 if __name__ == '__main__':
